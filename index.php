d8d9743b037f66b9273e8e8ed2f7a1ce64287856
Edik <Zolotukhin.e@list.ru>
2015-05-24 23:38:55 +0300

    Form registration

--- index.php
+++ index.php
@@ -1,3 +1,37 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset="utf-8"/>
+        <title>Tutorial: Super Simple Registration System With PHP & MySQL</title>
+
+        <!-- Главный CSS файл -->
+        <link href="assets/css/style.css" rel="stylesheet" />
+
+        <!--[if lt IE 9]>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+    </head>
+
+    <body>
+
+        <form id="login-register" method="post" action="index.php">
+
+            <h1>Login or Register</h1>
+
+            <input type="text" placeholder="your@email.com" name="email" 
+            autofocus />
+            <p>Enter your email address above and we will send <br />you a login link.</p>
+
+            <button type="submit">Login / Register</button>
+
+            <span></span>
+
+        </form>
+
+        <!-- Подключение яваскрипт -->
+        <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script src="assets/js/script.js"></script>
 <?php
 require_once './common/init.php';
 ?>
@@ -6,4 +40,9 @@ require_once './common/init.php';
     <a href="/register.php">Регистрация пользователя</a><br>
     <a href="/auth.php">Авторизация пользователя</a>
 </div>
+
+    </body>
+</html>
+
+
 
