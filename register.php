df4e6435feb2013c7ea40096183f4a8b5277012f
Edik <Zolotukhin.e@list.ru>
2015-05-24 23:41:26 +0300

    create form

--- register.php
+++ register.php
@@ -4,11 +4,18 @@ require_once './common/init.php';
 include './templates/header.php';
 ?>
 
+require_once 'includes/main.php';
+
+$user = new User();
+
+if(!$user->loggedIn()){
+    redirect('index.php');
+}
+
 Регистрация:<br>
 <form method="post" action="">
     <input type="text" name="username" placeholder="Insert Username" value=""/>
 </form>
-
 
 
 <?php
