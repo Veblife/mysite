62fc2a06c4c5c75a6a1fd550c2ac1a239a613f93
Veblife <vebife@gmail.com>
2015-05-27 20:04:34 +0300

    added validation fields in form

--- index.php
+++ index.php
@@ -1,39 +1,6 @@
-<!DOCTYPE html>
-<html>
-
-    <head>
-        <meta charset="utf-8"/>
-        <title>Tutorial: Super Simple Registration System With PHP & MySQL</title>
-
-        <!-- Главный CSS файл -->
-        <link href="assets/css/style.css" rel="stylesheet" />
-
-        <!--[if lt IE 9]>
-        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-        <![endif]-->
-    </head>
-
-    <body>
-
-        <form id="login-register" method="post" action="index.php">
-
-            <h1>Login or Register</h1>
-
-            <input type="text" placeholder="your@email.com" name="email" 
-            autofocus />
-            <p>Enter your email address above and we will send <br />you a login link.</p>
-
-            <button type="submit">Login / Register</button>
-
-            <span></span>
-
-        </form>
-
-        <!-- Подключение яваскрипт -->
-        <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-        <script src="assets/js/script.js"></script>
 <?php
 require_once './common/init.php';
+require_once './templates/header.php';
 ?>
 <div>
     <?php /* Владик, почитай про относительніе и абсолютніе пути адресации */ ?>
@@ -41,8 +8,7 @@ require_once './common/init.php';
     <a href="/auth.php">Авторизация пользователя</a>
 </div>
 
-    </body>
-</html>
-
+<?php
+require_once './templates/footer.php';
 
 
--- register.php
+++ register.php
@@ -1,20 +1,59 @@
 <?php
 require_once './common/init.php';
 
-include './templates/header.php';
-?>
+$message = '';
 
-require_once 'includes/main.php';
 
-$user = new User();
-
-if(!$user->loggedIn()){
-    redirect('index.php');
+if(count($_POST) && isset($_POST['register-user'])){
+    $errors = array();
+    $validData = array();
+    
+    // check username
+    if(isset($_POST['username']) && $_POST['username'] !== ''){
+        $validData['username'] = $_POST['username'];
+    }else{
+        $errors[] = 'Ошибка username';
+    }
+    
+    // check email
+    if(isset($_POST['email']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+        $validData['email'] = $_POST['email'];
+    }else{
+        $errors[] = 'Ошибка email';
+    }    
+    
+    if( isset($_POST['password']) && strlen($_POST['password']) >= 6){
+        $validData['password'] = $_POST['password'];
+    }else{
+        $errors[] = 'Ошибка password';
+    }   
+    
+    if(count($errors)){
+        $message = implode('<br>', $errors);
+    }
+    echo 'register zapros<br>';
+    var_dump($validData);
 }
 
-Регистрация:<br>
+
+
+
+
+include './templates/header.php';
+?>
+<h3>Регистрация:</h3>
+
+<?php if($message): ?>
+    <div style="border: 1px dotted red;">
+        <span style="color: red;"><?= $message; ?></span>
+    </div>
+<?php endif; ?>
+
 <form method="post" action="">
-    <input type="text" name="username" placeholder="Insert Username" value=""/>
+    <input type="text" name="username" placeholder="Username" value="Vasya" required /><br>
+    <input type="email" name="email" placeholder="Email" value="test@mail.ru" required/><br>
+    <input type="password" name="password" placeholder="Password" value="123456" required/><br>
+    <input type="submit" name="register-user" value="Click me, please..." />
 </form>
 
 
